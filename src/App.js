70442897a4fc6ec0f598051ed49086b6927fb19d
enzo <Eynzof@gmail.com>
2022-11-15 17:14:54 +0800

    use grid layout

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 function App() {
     return (
         <div>
-            <header className="sticky top-0 py-5 w-full bg-indigo-50 border-b-2 border-indigo-300 ...">Sticky header</header>
-
-            <main>
-                <div className="flex" style={{minHeight: '120vh'}}>
-                    <div className="flex-1 bg-indigo-50 border-r-2 border-indigo-300 p-4">Left Screen</div>
+            <header className="sticky top-0 h-16 w-full bg-indigo-50 border-b-2 border-indigo-300 ...">Sticky header
+            </header>
+            <main >
+                <div className="grid grid-cols-2" style={{minHeight: '120vh', gridTemplateColumns: "3fr 1fr"}}>
+                    <div className="flex-1 bg-indigo-50 border-r-2 border-indigo-300 p-4" >Left Screen</div>
                     <div className="flex-1 bg-indigo-50 p-4">Right Screen</div>
                 </div>
             </main>
