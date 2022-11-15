93412b25b53bc1822ce588ee5a4144df3f464a4d
enzo <Eynzof@gmail.com>
2022-11-15 17:29:06 +0800

    initial responsive layout

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ function App() {
         <div>
             <header className="sticky top-0 h-16 w-full bg-indigo-50 border-b-2 border-indigo-300 ...">Sticky header
             </header>
-            <main >
-                <div className="grid grid-cols-2" style={{minHeight: '120vh', gridTemplateColumns: "3fr 1fr"}}>
-                    <div className="flex-1 bg-indigo-50 border-r-2 border-indigo-300 p-4" >Left Screen</div>
-                    <div className="flex-1 bg-indigo-50 p-4">Right Screen</div>
+            <main>
+                <div className="grid "
+                     style={{minHeight: '120vh'}} id="post">
+                    <div className="flex-1 bg-indigo-50 border-r-2 border-indigo-300 p-6">Left Screen</div>
+                    <div className="flex-1 bg-indigo-50 p-6">Right Screen</div>
                 </div>
             </main>
         </div>
