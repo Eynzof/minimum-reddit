def1bf009262b3e61007274aa513a4d584c50718
enzo <Eynzof@gmail.com>
2022-11-15 18:32:52 +0800

    basic component structure

---
+++ src/features/cards/Card.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+function mapStateToProps(state) {
+    return {};
+}
+
+function mapDispatchToProps(dispatch) {
+    return {};
+}
+
+class Card extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default connect(
+    mapStateToProps,
+)(Card);
---
+++ src/features/categories/Categories.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Categories extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+Categories.propTypes = {};
+
+export default Categories;
