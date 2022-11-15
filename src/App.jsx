a072e82870937d18b4ac55aa51076a30b704bbeb
enzo <Eynzof@gmail.com>
2022-11-15 19:30:53 +0800

    intialize axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import './App.css';
 import Card from "./features/cards/Card";
 import Cards from "./features/cards/Cards";
+import {useEffect} from "react";
+import axios from "axios";
+
+const baseURL = "https://www.reddit.com/r/popular.json";
 
 function App() {
+
+
+    useEffect(()=> {
+        axios.get(baseURL).then((resp)=> {
+            const data = resp.data.data;
+            const posts = data.children;
+            console.log(posts);
+            if(posts.length === 0) {
+                console.log("No posts fetched")
+            }
+        })
+    },[])
+
     return (
         <div className="flex flex-col items-center">
             <header id="header"
@@ -20,7 +37,7 @@ function App() {
                     <Cards />
                 </div>
                 <aside className="flex-1 p-2 card" id="categories">
-                    <p2>Sub Reddit</p2>
+                    <p>Sub Reddit</p>
                     <ul>
                         <li>1</li>
                         <li>1</li>
