25d45e072978cf94f1072c0f540f517c761f6782
enzo <Eynzof@gmail.com>
2022-11-15 20:11:21 +0800

    fix post image

--- src/features/cards/Card.jsx
+++ src/features/cards/Card.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
+function checkURL(url) {
+    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
 
 const Card = (props) => {
-    const post = props.post;
+    const post = props.post.data;
     console.log("Card received post: ", post);
+    const url = post['url'];
+    const valid_url = checkURL(url)? url : null;
+
     return (
         <div className="card">
             <div className="post-vote-container"></div>
             <div className="post-container">
-                <h3 className="post-title">{props.post.data.title}</h3>
-                <div className="post-image-container"></div>
+                <h3 className="post-title">{post.title}</h3>
+                <div className="post-image-container">
+                    {valid_url?<img alt="post" src={valid_url}/> : null}
+                </div>
                 <div className="post-details">
                     <span className="post-author"></span>
                     <span className="post-date"></span>
