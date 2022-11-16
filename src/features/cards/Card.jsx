c5788298710589d3de5c78a64b3c402f1ad862e0
enzo <Eynzof@gmail.com>
2022-11-16 19:14:51 +0800

    format score

--- src/features/cards/Card.jsx
+++ src/features/cards/Card.jsx
@@ -1,28 +1,37 @@
 import React from 'react'
-import { UpOutlined, DownOutlined } from "@ant-design/icons";
+import {DownOutlined, UpOutlined} from "@ant-design/icons";
 import {Button} from "antd";
 
 function checkURL(url) {
-    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+    return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
+
+function m(n, d) {
+    let x = ('' + n).length;
+    let p = Math.pow
+    d = p(10, d);
+    x -= x % 3
+    return Math.round(n * d / p(10, x)) / d + " kMGTPE"[x / 3]
 }
 
 const Card = (props) => {
     const post = props.post.data;
     console.log("Card received post: ", post);
     const url = post['url'];
-    const valid_url = checkURL(url)? url : null;
+    const valid_url = checkURL(url) ? url : null;
 
     return (
         <div className="post-card flex flex-row">
             <div className="post-vote-container flex flex-col items-center">
-                <Button type="primary" icon={<UpOutlined />} size='small' className="flex justify-center items-center"/>
-                <span>0</span>
-                <Button type="primary" icon={<DownOutlined />} size='small' className="flex justify-center items-center"/>
+                <Button type="primary" icon={<UpOutlined/>} size='small' className="flex justify-center items-center"/>
+                <span className="post-score">{m(post.score, 1)}</span>
+                <Button type="primary" icon={<DownOutlined/>} size='small'
+                        className="flex justify-center items-center"/>
             </div>
             <div className="post-container">
                 <h3 className="post-title">{post.title}</h3>
                 <div className="post-image-container">
-                    {valid_url?<img alt="post" src={valid_url} style={{maxHeight: "512px"}}/> : null}
+                    {valid_url ? <img alt="post" src={valid_url} style={{maxHeight: "512px"}}/> : null}
                 </div>
                 <div className="post-details">
                     <span className="post-author"></span>
