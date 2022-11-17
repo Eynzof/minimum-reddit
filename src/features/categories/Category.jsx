5a85a5c93f4fe65ed37a0502cd86c596a14f82e2
enzo <Eynzof@gmail.com>
2022-11-17 13:16:47 +0800

    set proptypes

--- src/features/categories/Category.jsx
+++ src/features/categories/Category.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-
-const Category = (props) => {
-    console.log('component category received: ', props.subreddit)
+const Category = ({subreddit, handleClick, selected}) => {
+    // console.log('component category received: ', subreddit)
+    const url = subreddit.url;
     return (
-        <div className={props.selected ? "selected-subreddit subreddit-card" : "subreddit-card"}>
-            <button type="button" onClick={props.handleClick}
-                    value={props.subreddit.display_name}>{props.subreddit.display_name}</button>
+        <div className={selected ? "selected-subreddit subreddit-card" : "subreddit-card"}>
+            <button type="button" onClick={handleClick}
+                    value={subreddit.display_name}>{subreddit.display_name}</button>
         </div>
     );
 }
 
-Category.propTypes = {};
+Category.propTypes = {
+    subreddit: PropTypes.object,
+    handleClick: PropTypes.func,
+    selected: PropTypes.bool
+};
 
 export default Category;
