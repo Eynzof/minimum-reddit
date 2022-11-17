d9091f800fb432bdcd8afc2ce643db87503c6cb4
enzo <Eynzof@gmail.com>
2022-11-17 19:09:20 +0800

    skeleton

--- src/features/cards/CardSkeleton.jsx
+++ src/features/cards/CardSkeleton.jsx
@@ -4,28 +4,22 @@ import {DownOutlined, UpOutlined} from "@ant-design/icons";
 
 const CardSkeleton = () => {
     return (
-        <div className="flex flex-col">
-            <div className="post-card flex flex-row">
-                <div className="post-vote-container flex flex-col items-center">
-                    <Button type="primary" icon={<UpOutlined/>} size='small' className="flex justify-center items-center"/>
-                    <span className="post-score">
-                        <Skeleton active />
-                    </span>
-                    <Button type="primary" icon={<DownOutlined/>} size='small'
-                            className="flex justify-center items-center"/>
+        <div className="post-card flex flex-row">
+            <div className="post-vote-container flex flex-col items-center">
+                <Button type="ghost" icon={<UpOutlined/>} size='small' className="flex justify-center items-center" style={{border: "none"}}/>
+                <span className="post-score">0</span>
+                <Button type="ghost" icon={<DownOutlined/>} size='small'
+                        className="flex justify-center items-center" style={{border: "none"}}/>
+            </div>
+            <div className="post-container">
+                <h3 className="post-title"><Skeleton/></h3>
+                <div className="post-image-container">
+                    <Skeleton/>
                 </div>
-                <div className="post-container">
-                    <h3 className="post-title">
-                        <Skeleton />
-                    </h3>
-                    <div className="post-image-container">
-                        <Skeleton.Avatar active shape='circle' />
-                    </div>
-                    <div className="post-details">
-                        <span className="post-author"></span>
-                        <span className="post-date"></span>
-                        <div className="post-comments-container"></div>
-                    </div>
+                <div className="post-details">
+                    <span className="post-author"></span>
+                    <span className="post-date"></span>
+                    <div className="post-comments-container"></div>
                 </div>
             </div>
         </div>
