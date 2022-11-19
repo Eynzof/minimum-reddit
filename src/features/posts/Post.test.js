c1e58cbb8ef266f0460c658e4d688fae53ffa214
enzo <Eynzof@gmail.com>
2022-11-19 22:58:23 +0800

    refactor test

--- src/features/posts/Post.test.js
+++ src/features/posts/Post.test.js
@@ -1,16 +1,8 @@
 import { m } from './Post';
 
-test('1,023 to 1k', () => {
+test('post number to formatted string', () => {
     expect(m(1023)).toBe('1k');
-})
-
-test('500,000 to 500k', () => {
-    expect(m(500000)).toBe('500k');
-})
-
-test('15 to 15', () => {
-    expect(m(15)).toBe('15');
-})
-test('-1 to 0', () => {
     expect(m(-1)).toBe('0');
+    expect(m(500000)).toBe('500k');
+    expect(m(15)).toBe('15');
 })
