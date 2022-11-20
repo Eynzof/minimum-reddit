b2fd000f61b1084598304b821d7ba7daa9a4b2eb
enzo <Eynzof@gmail.com>
2022-11-20 17:10:20 +0800

    parse comment

--- src/features/posts/comments/Comment.jsx
+++ src/features/posts/comments/Comment.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import parse from "html-react-parser";
 
 const Comment = ({comment}) => {
     console.log(comment)
 
-    const c = comment.data;
+    const parse = require('html-react-parser')
 
-    return (<div className="post-comment">
+    const c = comment.data;
+    const body = parse(parse(c.body_html))
+
+    return (<div className="post-comment card" style={{padding: "1rem", marginBottom: '1rem'}}>
         <div className="comment-meta flex flex-row justify-between">
             {/*<img>*/}
             <p className="comment-author">{c.author}</p>
-            <p className="comment-time">3 hours ago</p>
+            <p className="comment-time">{c.created}</p>
         </div>
         <div>
-            <p className="comment">{c.body}</p>
+            <p className="comment" style={{ wordWrap: 'break-word'}}  >{body}</p>
         </div>
     </div>)
 }
