0b91d7899ebbb3943e40d309317c2ef08c3e7679
enzo <Eynzof@gmail.com>
2022-11-20 17:42:21 +0800

    little fix

--- src/features/posts/comments/CommentSkeleton.jsx
+++ src/features/posts/comments/CommentSkeleton.jsx
@@ -36,27 +36,5 @@ export const CommentSkeleton = () => {
             </div>
             <div className="comment" style={{wordWrap: 'break-word'}}><Skeleton/></div>
         </div>
-        <div className="post-comment card" style={{
-            padding: "1rem",
-            marginBottom: '1rem',
-            boxShadow: 'none',
-            border: '1px solid var(--color-border)'
-        }}>
-            <div className="comment-meta flex flex-row justify-between">
-                <Skeleton/>
-            </div>
-            <div className="comment" style={{wordWrap: 'break-word'}}><Skeleton/></div>
-        </div>
-        <div className="post-comment card" style={{
-            padding: "1rem",
-            marginBottom: '1rem',
-            boxShadow: 'none',
-            border: '1px solid var(--color-border)'
-        }}>
-            <div className="comment-meta flex flex-row justify-between">
-                <Skeleton/>
-            </div>
-            <div className="comment" style={{wordWrap: 'break-word'}}><Skeleton/></div>
-        </div>
     </div>)
 }
--- src/features/posts/comments/Comments.jsx
+++ src/features/posts/comments/Comments.jsx
@@ -8,7 +8,6 @@ const Comments = ({comments}) => {
     const c = comments[0].data.slice(0,10);
     return (<div className="post-comments mt-2">
         {c.map(comment => <Comment key={comment.data.id} comment={comment}/>)}
-            {/*<Comment key={comments[0].id} comment={comments[0].data}/>*/}
     </div>)
 }
 
