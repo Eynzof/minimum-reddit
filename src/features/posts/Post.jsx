d13c1dfcfc59ca3c9f6da1550f84e6ad8ba4d325
enzo <Eynzof@gmail.com>
2022-11-20 14:25:10 +0800

    add modal

--- src/features/posts/Post.jsx
+++ src/features/posts/Post.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {CommentOutlined, DownOutlined, UpOutlined} from "@ant-design/icons";
-import {Button} from "antd";
+import {Button, Modal} from "antd";
 
 function checkURL(url) {
     return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
@@ -22,6 +22,19 @@ const Post = ({post}) => {
     const url = post['url'];
     const valid_url = checkURL(url) ? url : null;
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div className="post-card flex flex-row">
             <div className="post-vote-container flex flex-col items-center">
@@ -37,7 +50,11 @@ const Post = ({post}) => {
                     <span className="post-date"></span>
                     <div className="post-comments-container"></div>
                 </div>
-                <h3 className="post-title">{post.title}</h3>
+
+                <Button type="link" onClick={showModal} style={{padding: '0'}}>
+                    <h3 className="post-title">{post.title}</h3>
+                </Button>
+                <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>content</Modal>
                 <div className="post-image-container">
                     {valid_url ? <img alt="post" src={valid_url} style={{maxHeight: "512px"}}/> : null}
                 </div>
