307c11cd3263cec62a39d4407e51909c1a4e2fa0
enzo <Eynzof@gmail.com>
2022-11-20 17:25:35 +0800

    time formatted

--- src/features/posts/comments/Comment.jsx
+++ src/features/posts/comments/Comment.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import './Comment.css'
+
+const parse = require('html-react-parser')
+const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
+
 const Comment = ({comment}) => {
-    console.log(comment)
-
-    const parse = require('html-react-parser')
-
     const c = comment.data;
     const body = parse(parse(c.body_html))
+
+    const t = dayjs.unix(c.created);
 
     return (<div className="post-comment card" style={{
         padding: "1rem",
@@ -17,11 +21,9 @@ const Comment = ({comment}) => {
         <div className="comment-meta flex flex-row justify-between">
             {/*<img>*/}
             <p className="comment-author">{c.author}</p>
-            <p className="comment-time">{c.created}</p>
+            <p className="comment-time">{t.fromNow()}</p>
         </div>
-        <div>
-            <p className="comment" style={{wordWrap: 'break-word'}}>{body}</p>
-        </div>
+        <div className="comment" style={{wordWrap: 'break-word'}}>{body}</div>
     </div>)
 }
 
