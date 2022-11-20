4190b82537f4554fd8355c3a24b0b50c610d9495
enzo <Eynzof@gmail.com>
2022-11-20 17:16:18 +0800

    refactor css

--- src/features/posts/comments/Comment.jsx
+++ src/features/posts/comments/Comment.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import parse from "html-react-parser";
-
+import './Comment.css'
 const Comment = ({comment}) => {
     console.log(comment)
 
@@ -9,14 +8,19 @@ const Comment = ({comment}) => {
     const c = comment.data;
     const body = parse(parse(c.body_html))
 
-    return (<div className="post-comment card" style={{padding: "1rem", marginBottom: '1rem'}}>
+    return (<div className="post-comment card" style={{
+        padding: "1rem",
+        marginBottom: '1rem',
+        boxShadow: 'none',
+        border: '1px solid var(--color-border)'
+    }}>
         <div className="comment-meta flex flex-row justify-between">
             {/*<img>*/}
             <p className="comment-author">{c.author}</p>
             <p className="comment-time">{c.created}</p>
         </div>
         <div>
-            <p className="comment" style={{ wordWrap: 'break-word'}}  >{body}</p>
+            <p className="comment" style={{wordWrap: 'break-word'}}>{body}</p>
         </div>
     </div>)
 }
